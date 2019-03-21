7d03901288f4190e0b3a41b174841e2f464068df
thevikrampanwar <thevikramlpanwar@gmail.com>
2019-03-21 11:46:43 +0530

    Updates

---
+++ precache-manifest.35ee4c9ca8be3b6b14d189b81ec13994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e14a05a4702987c8737",
+    "url": "vikrampanwar999.github.io/react-crud/static/js/runtime~main.1e14a05a.js"
+  },
+  {
+    "revision": "910c9e9199c885723ce6",
+    "url": "vikrampanwar999.github.io/react-crud/static/js/main.910c9e91.chunk.js"
+  },
+  {
+    "revision": "daaf546f5bda08ee30ee",
+    "url": "vikrampanwar999.github.io/react-crud/static/js/1.daaf546f.chunk.js"
+  },
+  {
+    "revision": "910c9e9199c885723ce6",
+    "url": "vikrampanwar999.github.io/react-crud/static/css/main.740d0343.chunk.css"
+  },
+  {
+    "revision": "c373baa08c89a871d368a1f6bc5dc6b3",
+    "url": "vikrampanwar999.github.io/react-crud/index.html"
+  }
+];
